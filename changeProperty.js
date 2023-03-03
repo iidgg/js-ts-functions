8ada50c9790b63843ad603359d063e8a5cb359b8
id <105822145+iidgg@users.noreply.github.com>
2023-03-04 00:00:03 +0300

    Update changeProperty.js

--- changeProperty.js
+++ changeProperty.js
@@ -7,6 +7,14 @@ function changeProperty(arr, oldProp, newProp) {
 // the old property name,
 // and the new property name.
 
-//  It then uses the map() method to create a new array with the same objects
+// It then uses the map() method to create a new array with the same objects
 // but with the new property name and the old value.
 // The spread operator (...) is used to copy all the other properties of the object.
+
+// Test
+
+const array = [{name: "John", age: 30}, {name: "Jane", age: 25}];
+const newArray = changeProperty(array, "age", "years");
+
+console.log(newArray);
+// Expected output: [{name: "John", years: 30}, {name: "Jane", years: 25}]
